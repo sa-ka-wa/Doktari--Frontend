672b81a2908a76055797e0a1db2761203032215e
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-10-25 10:08:53 +0300

    frontend structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Your T-Shirt Customization App</h1>
+      {/* Add your router and main layout here */}
+    </div>
+  );
+}
+
+export default App;
